3bfbc03eb81f031107d6538033906d30d10fa61c
Ma7rous <ahmedmahros425@gmail.com>
2022-10-16 22:02:29 +0200

    Version 0.0.1

---
+++ Sources/IMChat.xcframework/ios-arm64_x86_64-simulator/IMChat.framework/Headers/IMChat.h
@@ -0,0 +1,18 @@
+//
+//  IMChat.h
+//  IMChat
+//
+//  Created by Ma7rous on 16/10/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IMChat.
+FOUNDATION_EXPORT double IMChatVersionNumber;
+
+//! Project version string for IMChat.
+FOUNDATION_EXPORT const unsigned char IMChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IMChat/PublicHeader.h>
+
+
